f2587d55a4800cacc1754522deb73c065cb2f0d0
Shivanshu Prajapati <shivanshuprajapati212@gmail.com>
2026-04-10 17:47:00 +0530

    yoo, my own tmux starter

--- main.sh
+++ main.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 
-tmux new -s nvim -d
-tmux send-keys -t nvim 'nvim' C-m
-tmux attach -t nvim
+name=$(basename "$PWD")
+
+tmux new -s $name -d
+tmux send-keys -t $name 'nvim .' C-m
+tmux rename-window -t $name nvim
+
+tmux new-window -t $name
+tmux rename-window -t $name zsh 
+tmux send-keys -t $name 'neofetch' C-m
+
+tmux new-window -t $name
+tmux rename-window -t $name git 
+tmux send-keys -t $name 'git status' C-m
+
+tmux new-window -t $name
+tmux rename-window -t $name other 
+
+tmux select-window -t $name:1
+ 
+tmux attach -t $name 
